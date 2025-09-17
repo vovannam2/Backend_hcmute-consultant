5e58abf2f193dea3d8d061cb1e3cd80007d41ee7
Vo Van Nam <vovannam220304@gmail.com>
2025-09-17 09:58:31 +0700

    Authentication & Profile

---
+++ src/routes/authRoutes.js.LOCAL.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const {
+  registerRequest,
+  registerVerify,
+  login,
+  forgotPassword,
+  verifyOtp,
+  resetPassword,
+} = require("../controllers/common/authController");
+
+const router = express.Router();
+
+// Register (OTP)
+router.post("/register/request", registerRequest);
+router.post("/register/verify", registerVerify);
+router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
+router.post("/verify-otp", verifyOtp);
+router.post("/reset-password", resetPassword);
+
+// Forgot Password (OTP)
+//router.post("/forgot/request", forgotPasswordRequest);
+// router.post("/forgot/verify", forgotPasswordVerify);
+
+module.exports = router;
